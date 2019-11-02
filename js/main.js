b42da029c20c7445ad05f5a87937697ac50d6d4f
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2019-11-02 16:05:56 +0000

    Test 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+var keys;
+var player;
+
 function main() {
     console.log("main()");
 
     var config = {
         type: Phaser.AUTO,
         parent: 'my-game',
-        width: 400,
-        height: 500,
+        width: 1000,
+        height: 800,
         physics: {
             default: 'arcade',
             arcade: {
@@ -24,10 +27,28 @@ function main() {
 
 function preload() {
     this.load.image('player', 'assets/tiny_ship.png');
+    this.load.image('laser', 'assets/laser.png');
 }
 
 function create() {
-    var player = phaser.physics.add.sprite(200, 250, 'player');
+    
+    player = this.physics.add.sprite(200, 250, 'player');
+    player.setCollideWorldBounds(true);
+    
+    var thing = this.physics.add.sprite(200, 300, 'laser');
+
+    this.physics.add.overlap(player, thing, meme, null, this);
+    
+    keys = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+    if (keys.up.isDown) { player.y = player.y - 1; }
+    if (keys.down.isDown) { player.y = player.y + 1; }
+    if (keys.left.isDown) { player.x = player.x - 1; }
+    if (keys.right.isDown) { player.x = player.x + 1; }
+}
+
+function meme() {
+    console.log("Meme");
+}
